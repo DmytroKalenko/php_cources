0398c4bbf91cb41e4e187f3ecf10d9a27086cd72
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2022-02-24 00:16:27 +0100

    redundant code removed

--- app/views/customer/list.php
+++ app/views/customer/list.php
@@ -1,15 +1,15 @@
-<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
-    <?php $_SERVER['PHP_SELF'] ?>
-    <select name='sortfirst'>
-        <option <?php echo filter_input(INPUT_POST, 'sortname') === 'name_ASC' ? 'selected' : ''; ?>
-                value="name_ASC">від А до Я
-        </option>
-        <option <?php echo filter_input(INPUT_POST, 'sortname') === 'name_DESC' ? 'selected' : ''; ?>
-                value="name_DESC">від Я до А
-        </option>
-    </select>
-    <input type="submit" value="Submit">
-</form>
+<!--<form method="POST" action="--><?php //$_SERVER['PHP_SELF']; ?><!--">-->
+<!---->
+<!--    <select name='sortfirst'>-->
+<!--        <option --><?php //echo filter_input(INPUT_POST, 'sortname') === 'name_ASC' ? 'selected' : ''; ?>
+<!--                value="name_ASC">від А до Я-->
+<!--        </option>-->
+<!--        <option --><?php //echo filter_input(INPUT_POST, 'sortname') === 'name_DESC' ? 'selected' : ''; ?>
+<!--                value="name_DESC">від Я до А-->
+<!--        </option>-->
+<!--    </select>-->
+<!--    <input type="submit" value="Submit">-->
+<!--</form>-->
 
 <div class="customer">
     <p><?= \Core\Url::getLink('/customer/add', 'Додати клієнта'); ?></p>
@@ -27,13 +27,13 @@ foreach ($customers as $customer)  :
         <span class="phoneNumber">Номер телефону: <a href="tel:<?php echo $customer['telephone'] ?>"><?php echo $customer['telephone'] ?></a></span>
         <span class="mail">Е-mail: <a href="mailto:"<?php echo $customer['email']?>"><?php echo $customer['email'] ?></a></span>
 
-        <div class="box">
-            <span id="delete__edit">
-                <?= \Core\Url::getLink('/customer/edit', 'Редагувати', array('id' => $customer['customer_id'])); ?>
-            </span>
-            <span id="delete__btn">
-                <?= \Core\Url::getLink('/customer/delete', 'Видалити', array('id' => $customer['customer_id'])); ?>
-            </span>
-        </div>
+<!--        <div class="box">-->
+<!--            <span id="delete__edit">-->
+<!--                --><?//= \Core\Url::getLink('/customer/edit', 'Редагувати', array('id' => $customer['customer_id'])); ?>
+<!--            </span>-->
+<!--            <span id="delete__btn">-->
+<!--                --><?//= \Core\Url::getLink('/customer/delete', 'Видалити', array('id' => $customer['customer_id'])); ?>
+<!--            </span>-->
+<!--        </div>-->
     </div>
 <?php endforeach; ?>
